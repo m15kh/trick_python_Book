513f49074668e3e7f5406629fdb632983c434e60
Mohammad <91499974+m15kh@users.noreply.github.com>
2023-07-23 19:16:17 +0330

    addd another exp clen and hard code

--- hardcoding_lambda.py
+++ hardcoding_lambda.py
@@ -1,6 +1,17 @@
-#hardcoding
+#hard coding
 a = list(filter(lambda x: x % 2 == 0, range(16)))
 print(a)
 #clean code
-b= [x for x in range(16) if x%2 == 0]
+b = [x for x in range(16) if x%2 == 0]
 print(b)
+
+#another example
+
+#hard coding
+
+a1 = list(filter(lambda x: x>4, range(10)))
+print(a1)
+
+
+b1 = [x for x in range(10) if x>4]
+print(b1)
